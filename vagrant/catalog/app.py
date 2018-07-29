6e7e942d0529af93ddf8bd86eadc68970a189785
shradhakatyal <skatyal2204@gmail.com>
2018-07-29 23:18:38 +0530

    item details page modified

--- vagrant/catalog/app.py
+++ vagrant/catalog/app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for
 from sqlalchemy import create_engine, and_
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Category, Item
+import string
 
 app = Flask(__name__)
 
@@ -30,13 +31,13 @@ def getItemsFromCategory(cat_name):
     return render_template('items.html', categories=categories, items=items)
 
 #To display information about a single item
-# @app.route('/catalog/<string:cat_name>/<string:item_title>')
-# def getItemDetails(cat_name, item_title):
-#     q = session.query(Category).filter_by(name=cat_name.title()).one()
-#     cat_id = q.id
-#     item = session.query(Item).filter_by(title = "Snowboard")
-    # print(item_title.title())
-    # return render_template('itemdetail.html', item=item)
+@app.route('/catalog/<string:cat_name>/<string:item_title>')
+def getItemDetails(cat_name, item_title):
+    # item = session.query(Item).join(Category).filter(Item.title == item_title.title()).filter(Category.name == cat_name.title()).one()
+    q = session.query(Category).filter_by(name=cat_name.title()).one()
+    cat_id = q.id
+    item = session.query(Item).filter_by(cat_id=cat_id).filter_by(title=item_title.title()).one()
+    return render_template('itemdetail.html', item=item)
 
 if __name__ == '__main__':
     app.debug = True
