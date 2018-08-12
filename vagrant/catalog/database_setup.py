7f38a97f21f0758aba6369570a1a30c16e723817
shradhakatyal <skatyal2204@gmail.com>
2018-08-12 23:50:06 +0530

    gconnect page added

--- vagrant/catalog/database_setup.py
+++ vagrant/catalog/database_setup.py
@@ -1,8 +1,9 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String, create_engine
+from sqlalchemy import Column, ForeignKey, Integer, String, create_engine, Boolean, Text, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
+import datetime
 
 Base = declarative_base()
 
@@ -26,12 +27,12 @@ class Item(Base):
 class User(Base):
     __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    # email = Column(String(100), unique=True, nullable=False)
-    # name = Column(String(100), nullable=True)
-    # avatar = Column(String(200))
-    # active = Column(Boolean, default=False)
-    # tokens = Column(Text)
-    # created_at = Column(DateTime, default=datetime.datetime.utcnow())
+    email = Column(String(100), unique=True, nullable=False)
+    name = Column(String(100), nullable=True)
+    avatar = Column(String(200))
+    active = Column(Boolean, default=False)
+    tokens = Column(Text)
+    created_at = Column(DateTime, default=datetime.datetime.utcnow())
 
 
 engine = create_engine(
